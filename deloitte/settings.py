e1df1d17fbaba7b92ead1b9646238410dd44bd6f
Daniel García Alvarado <danigarciaalva@hotmail.com>
2015-05-26 19:16:05 -0500

    Statics served

--- deloitte/settings.py
+++ deloitte/settings.py
@@ -81,4 +81,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+MEDIA_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__), '../media/').replace('\\', '/'))
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__), '../static/').replace('\\', '/'))
 STATIC_URL = '/static/'
